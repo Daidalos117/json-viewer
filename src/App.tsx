16b5526bea67898543ba6f69a15525dba03d15e1
Roman Rajchert <rajchert.roman@gmail.com>
2021-07-11 18:29:31 +0200

    Add basic input possibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import exampleData from './exampleData.json'
-import {JSONRender} from "components/renderers/JSONRender";
+import React, { useState } from 'react';
+import { ChakraProvider, Alert, AlertIcon, AlertTitle, CloseButton } from '@chakra-ui/react';
+
+import { JSONRender } from 'components/renderers/JSONRender';
+import { Textarea, Button, Container, Box, Heading } from '@chakra-ui/react';
 
 function App() {
-
-
+    const [jsonValue, setJsonValue] = useState<null | string>(null);
+    const [json, setJson] = useState<null | string>(null);
+    const [error, setError] = useState<null | string>(null);
 
     return (
         <ChakraProvider>
-            <div className="App">
-                <JSONRender JSONData={JSON.stringify(exampleData)} />
-            </div>
+            <Box className="App" minHeight="100vh">
+                {!json && (
+                    <Container>
+                        <Heading mb={6} mt={8}>
+                            Paste Your JSON here to format
+                        </Heading>
+                        <form
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                setJson(jsonValue);
+                            }}
+                        >
+                            <Textarea
+                                placeholder="Paste JSON here"
+                                rows={20}
+                                onChange={(e) => setJsonValue(e.target.value)}
+                            />
+
+                            <Button type="submit" colorScheme="green" size="lg" mt={2}>
+                                Submit
+                            </Button>
+                        </form>
+
+                        {error && (
+                            <Alert status="error" mt={4}>
+                                <AlertIcon />
+                                <AlertTitle mr={2}>{error}</AlertTitle>
+                                <CloseButton position="absolute" right="8px" top="8px" />
+                            </Alert>
+                        )}
+                    </Container>
+                )}
+
+                {json && (
+                    <JSONRender
+                        JSONData={json}
+                        onError={(error) => {
+                            setJson(null);
+                            setError(error);
+                        }}
+                    />
+                )}
+            </Box>
         </ChakraProvider>
     );
 }
--- src/components/renderers/JSONRender.tsx
+++ src/components/renderers/JSONRender.tsx
@@ -1,12 +1,25 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect } from 'react';
 import {RenderByType} from "components/renderers/RenderByType";
 
 interface Props {
     JSONData: string;
+    onError: (error: string) => void;
 }
 
-export const JSONRender: React.FC<Props> = ({ JSONData }) => {
-    const data = useMemo(() => JSON.parse(JSONData), [JSONData]);
-    console.log({data})
-    return <RenderByType data={data} level={0} />;
+export const JSONRender: React.FC<Props> = ({ JSONData, onError }) => {
+
+    const data = useMemo(() => {
+        try {
+            return JSON.parse(JSONData)
+        }catch (e) {
+            return false;
+        }
+
+    }, [JSONData]);
+
+    useEffect(() => {
+        if(!data) onError("JSON not valid");
+    }, [data, onError]);
+
+    return data && <RenderByType data={data} level={0} />;
 };
